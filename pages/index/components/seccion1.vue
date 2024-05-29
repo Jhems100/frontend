6a2d58b8a7140c5c7ae02d2d5f85f4a1cd6e9594
Jhems <jdehervercalizaya9@gmail.com>
2024-05-29 18:28:59 -0400

    cambios4

--- pages/index/components/seccion1.vue
+++ pages/index/components/seccion1.vue
@@ -1,136 +1,136 @@
-<template>
-  <v-container>  
-    <!-- Características Principales -->
-    <v-row justify-center>
-  <v-col cols="12" class="text-center">
-    <h2 class="headline">Características Principales</h2>
-    <v-row>
-      <v-col cols="12" sm="4">
-        <v-list-item>
-          <v-list-item-icon>
-            <v-icon class="custom-icon">mdi-check-circle</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>Gestión Centralizada</v-list-item-title>
-            <v-list-item-subtitle>Unifica todo tu inventario en una sola plataforma</v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-      </v-col>
-      <v-col cols="12" sm="4">
-        <v-list-item>
-          <v-list-item-icon>
-            <v-icon class="custom-icon">mdi-check-circle</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>Registro de Transacciones</v-list-item-title>
-            <v-list-item-subtitle>Registra todas las transacciones de forma clara y precisa</v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-      </v-col>
-      <v-col cols="12" sm="4">
-        <v-list-item>
-          <v-list-item-icon>
-            <v-icon class="custom-icon">mdi-check-circle</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>Informes Financieros</v-list-item-title>
-            <v-list-item-subtitle>Genera informes financieros detallados para una mejor toma de decisiones</v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-      </v-col>
-    </v-row>
-  </v-col>
-</v-row>
-
-    <!-- Por qué elegir nuestro sistema de inventario -->
-    <v-container class="text-center d-flex flex-column align-center justify-center">
-  <v-row class="d-flex justify-center">
-    <v-col cols="12">
-      <h2 class="headline">¿Por qué elegir nuestro sistema de inventario?</h2><br>
-      <v-row class="d-flex justify-center">
-        <v-col cols="12" md="7">
-          <v-card class="custom-card">
-            <v-card-text>
-              <h3 class="headline">Eficiencia Mejorada</h3>
-              <p>Con funcionalidades automatizadas y una interfaz fácil de usar, nuestro sistema te permite ahorrar tiempo y recursos en la gestión de inventario.</p>
-            </v-card-text>
-          </v-card>
+  <template>
+    <v-container>  
+      <!-- Características Principales -->
+      <v-row justify-center>
+    <v-col cols="12" class="text-center">
+      <h2 class="headline">Características Principales</h2>
+      <v-row>
+        <v-col cols="12" sm="4">
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon class="custom-icon">mdi-check-circle</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Gestión Centralizada</v-list-item-title>
+              <v-list-item-subtitle>Unifica todo tu inventario en una sola plataforma</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
         </v-col>
-        <v-col cols="12" md="6">
-          <!-- Añadir más tarjetas para cada punto de venta -->
+        <v-col cols="12" sm="4">
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon class="custom-icon">mdi-check-circle</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Registro de Transacciones</v-list-item-title>
+              <v-list-item-subtitle>Registra todas las transacciones de forma clara y precisa</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-col>
+        <v-col cols="12" sm="4">
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon class="custom-icon">mdi-check-circle</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Informes Financieros</v-list-item-title>
+              <v-list-item-subtitle>Genera informes financieros detallados para una mejor toma de decisiones</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
         </v-col>
       </v-row>
     </v-col>
   </v-row>
-</v-container>
 
-    
-    <!-- Reposición Impecable -->
-  <v-container class="text-center">
-    <v-row>
+      <!-- Por qué elegir nuestro sistema de inventario -->
+      <v-container class="text-center d-flex flex-column align-center justify-center">
+    <v-row class="d-flex justify-center">
       <v-col cols="12">
-        <h2 class="headline">Reposición Impecable</h2>
-        <p>Mantén tu inventario siempre en óptimas condiciones con Izzistocks. <br>Nuestro plan maestro de producción y estrategias de reposición automática <br> aseguran que nunca te quedes sin stock. Automatizamos el seguimiento a <br> proveedores, enviando mensajes y confirmando órdenes de compra,<br> para una gestión eficiente y sin contratiempos.</p>
+        <h2 class="headline">¿Por qué elegir nuestro sistema de inventario?</h2><br>
+        <v-row class="d-flex justify-center">
+          <v-col cols="12" md="7">
+            <v-card class="custom-card">
+              <v-card-text>
+                <h3 class="headline">Eficiencia Mejorada</h3>
+                <p>Con funcionalidades automatizadas y una interfaz fácil de usar, nuestro sistema te permite ahorrar tiempo y recursos en la gestión de inventario.</p>
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col cols="12" md="6">
+            <!-- Añadir más tarjetas para cada punto de venta -->
+          </v-col>
+        </v-row>
       </v-col>
     </v-row>
-    </v-container>
+  </v-container>
 
-    <v-row justify="center">
-      <v-col cols="12" md="8" class="text-center">
-        <v-img src="/Users/jhemscalizayaicloud.com/Desktop/frontend/public/Captura de pantalla 2024-05-27 a la(s) 7.03.02 p. m..png" alt="Proceso de Reposición" max-width="100%"></v-img>
-      </v-col>
-    </v-row>
-    
-    <!-- Nuestro proyecto -->
+
+      <!-- Reposición Impecable -->
     <v-container class="text-center">
-    <v-row>
-      <v-col cols="12">
-        <h2 class="headline">Nuestro proyecto</h2>
-        <p>Nuestro sistema de inventario es la herramienta perfecta para optimizar la gestión<br> de tus productos y llevar tu negocio al siguiente nivel. Además de unificar tu inventario,<br> registrar transacciones y generar informes financieros, te proporciona una eficiencia<br> mejorada para ahorrar tiempo y recursos. ¡Únete a cientos de empresas que ya <br>han experimentado el poder de una gestión de inventario eficiente con nosotros!</p>
-      </v-col>
-    </v-row>
-  </v-container>
-  </v-container>
-</template>
+      <v-row>
+        <v-col cols="12">
+          <h2 class="headline">Reposición Impecable</h2>
+          <p>Mantén tu inventario siempre en óptimas condiciones con Izzistocks. <br>Nuestro plan maestro de producción y estrategias de reposición automática <br> aseguran que nunca te quedes sin stock. Automatizamos el seguimiento a <br> proveedores, enviando mensajes y confirmando órdenes de compra,<br> para una gestión eficiente y sin contratiempos.</p>
+        </v-col>
+      </v-row>
+      </v-container>
+
+      <v-row justify="center">
+        <v-col cols="12" md="8" class="text-center">
+          <v-img src="/Users/jhemscalizayaicloud.com/Desktop/frontend/public/Captura de pantalla 2024-05-27 a la(s) 7.03.02 p. m..png" alt="Proceso de Reposición" max-width="100%"></v-img>
+        </v-col>
+      </v-row>
+      
+      <!-- Nuestro proyecto -->
+      <v-container class="text-center">
+      <v-row>
+        <v-col cols="12">
+          <h2 class="headline">Nuestro proyecto</h2>
+          <p>Nuestro sistema de inventario es la herramienta perfecta para optimizar la gestión<br> de tus productos y llevar tu negocio al siguiente nivel. Además de unificar tu inventario,<br> registrar transacciones y generar informes financieros, te proporciona una eficiencia<br> mejorada para ahorrar tiempo y recursos. ¡Únete a cientos de empresas que ya <br>han experimentado el poder de una gestión de inventario eficiente con nosotros!</p>
+        </v-col>
+      </v-row>
+    </v-container>
+    </v-container>
+  </template>
 
 
-<script>
-export default {
-  name: 'InventoryPage',
-  // Puedes agregar lógica adicional aquí si es necesario
-}
-</script>
+  <script>
+  export default {
+    name: 'InventoryPage',
+    // Puedes agregar lógica adicional aquí si es necesario
+  }
+  </script>
 
-<style scoped>
-/* Estilos personalizados para esta página */
-.headline {
-  font-family: 'Comic Sans MS', cursive, sans-serif;
-  color: #3E2723;
-}
+  <style scoped>
+  /* Estilos personalizados para esta página */
+  .headline {
+    font-family: 'Comic Sans MS', cursive, sans-serif;
+    color: #3E2723;
+  }
 
-.custom-icon {
-  color: #8E44AD;
-}
+  .custom-icon {
+    color: #8E44AD;
+  }
 
-.custom-card {
-  background-color: #F3E5F5;
-  border: 1px solid #8E44AD;
-}
+  .custom-card {
+    background-color: #F3E5F5;
+    border: 1px solid #8E44AD;
+  }
 
-.v-list-item-title {
-  font-family: 'Comic Sans MS', cursive, sans-serif;
-}
+  .v-list-item-title {
+    font-family: 'Comic Sans MS', cursive, sans-serif;
+  }
 
-.v-list-item-subtitle {
-  color: #5D4037;
-}
+  .v-list-item-subtitle {
+    color: #5D4037;
+  }
 
-p {
-  font-family: 'Comic Sans MS', cursive, sans-serif;
-  color: #3E2723;
-}
+  p {
+    font-family: 'Comic Sans MS', cursive, sans-serif;
+    color: #3E2723;
+  }
 
-.text-center {
-  text-align: center;
-}
-</style>
+  .text-center {
+    text-align: center;
+  }
+  </style>
--- pages/products.vue
+++ pages/products.vue
@@ -1,196 +1,196 @@
-<template>
-  <v-app>
-    <v-app-bar app class="custom-app-bar">
-      <v-toolbar-title class="app-title">IzziStocks</v-toolbar-title>
-      <v-btn text class="nav-btn" @click="redirectTo('/')">Pagina Principal</v-btn>
-      <v-btn text class="nav-btn" @click="redirectTo('/products')">Planes y servicios</v-btn>
-      <v-btn text class="nav-btn" @click="redirectTo('/about')">sobre nosotros</v-btn>
-      <v-spacer></v-spacer>
-    </v-app-bar>
+  <template>
+    <v-app>
+      <v-app-bar app class="custom-app-bar">
+        <v-toolbar-title class="app-title">IzziStocks</v-toolbar-title>
+        <v-btn text class="nav-btn" @click="redirectTo('/')">Pagina Principal</v-btn>
+        <v-btn text class="nav-btn" @click="redirectTo('/products')">Planes y servicios</v-btn>
+        <v-btn text class="nav-btn" @click="redirectTo('/about')">sobre nosotros</v-btn>
+        <v-spacer></v-spacer>
+      </v-app-bar>
 
-    <div class="d-flex align-center justify-center fill-height">
-      <v-container>
-        <v-row class="mx-auto my-12" align="center" justify="center">
-          <v-col cols="12" class="text-center">
-            <h1 class="andale-mono">NUESTROS PLANES MENSUALES</h1>
-          </v-col>
-        </v-row>
-        <v-row class="mx-auto my-12" align="center" justify="center" style="width: 100%;">
-          <v-col cols="auto" class="card-col">
-            <v-card :disabled="loading" :loading="loading" max-width="374">
-              <template v-slot:loader="{ isActive }">
-                <v-progress-linear :active="isActive" color="deep-purple" height="4" indeterminate></v-progress-linear>
-              </template>
+      <div class="d-flex align-center justify-center fill-height">
+        <v-container>
+          <v-row class="mx-auto my-12" align="center" justify="center">
+            <v-col cols="12" class="text-center">
+              <h1 class="app-title">NUESTROS PLANES MENSUALES</h1>
+            </v-col>
+          </v-row>
+          <v-row class="mx-auto my-12" align="center" justify="center" style="width: 100%;">
+            <v-col cols="auto" class="card-col">
+              <v-card :disabled="loading" :loading="loading" max-width="374">
+                <template v-slot:loader="{ isActive }">
+                  <v-progress-linear :active="isActive" color="deep-purple" height="4" indeterminate></v-progress-linear>
+                </template>
 
-              <v-img height="250"
-                src="public/el concepto del sistema de control de inventario, el gerente profesional y el trabajador están verificando el suministro de bienes y existencias, la gestión de inventario con la demanda de bienes.jpeg"
-                cover></v-img>
+                <v-img height="250"
+                  src="public/el concepto del sistema de control de inventario, el gerente profesional y el trabajador están verificando el suministro de bienes y existencias, la gestión de inventario con la demanda de bienes.jpeg"
+                  cover></v-img>
 
-              <v-card-item>
-                <v-card-title>Plan Basico</v-card-title>
+                <v-card-item>
+                  <v-card-title>Plan Basico</v-card-title>
 
-                <v-card-subtitle>
-                  <span class="me-1">Sugerido</span>
+                  <v-card-subtitle>
+                    <span class="me-1">Sugerido</span>
 
-                  <v-icon color="error" icon="mdi-fire-circle" size="small"></v-icon>
-                </v-card-subtitle>
-              </v-card-item>
+                    <v-icon color="error" icon="mdi-fire-circle" size="small"></v-icon>
+                  </v-card-subtitle>
+                </v-card-item>
 
-              <v-card-text>
-                <v-row align="center" class="mx-0">
-                  <v-rating :model-value="4.5" color="amber" density="compact" size="small" half-increments
-                    readonly></v-rating>
+                <v-card-text>
+                  <v-row align="center" class="mx-0">
+                    <v-rating :model-value="4.5" color="amber" density="compact" size="small" half-increments
+                      readonly></v-rating>
 
-                  <div class="text-grey ms-4">
-                    4.5 (413)
+                    <div class="text-grey ms-4">
+                      4.5 (413)
+                    </div>
+                  </v-row>
+
+                  <div class="my-4 text-subtitle-1">
+                    7$ • La paz, Bolivia
                   </div>
-                </v-row>
 
-                <div class="my-4 text-subtitle-1">
-                  7$ • La paz, Bolivia
-                </div>
+                  <div>Sistema para rastrear, gestionar y optimizar niveles de inventario empresarial.</div>
+                </v-card-text>
 
-                <div>Sistema para rastrear, gestionar y optimizar niveles de inventario empresarial.</div>
-              </v-card-text>
+                <v-divider class="mx-4 mb-1"></v-divider>
 
-              <v-divider class="mx-4 mb-1"></v-divider>
+                <v-card-actions>
+                  <v-btn color="deep-purple-lighten-2" text="Comprar" block border @click="reserve"></v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
 
-              <v-card-actions>
-                <v-btn color="deep-purple-lighten-2" text="Comprar" block border @click="reserve"></v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-col>
+            <v-col cols="auto" class="card-col">
+              <v-card :disabled="loading" :loading="loading" max-width="374">
+                <template v-slot:loader="{ isActive }">
+                  <v-progress-linear :active="isActive" color="deep-purple" height="4" indeterminate></v-progress-linear>
+                </template>
 
-          <v-col cols="auto" class="card-col">
-            <v-card :disabled="loading" :loading="loading" max-width="374">
-              <template v-slot:loader="{ isActive }">
-                <v-progress-linear :active="isActive" color="deep-purple" height="4" indeterminate></v-progress-linear>
-              </template>
+                <v-img height="250"
+                  src="public/el concepto del sistema de control de inventario, el gerente profesional y el trabajador están verificando el suministro de bienes y existencias, la gestión de inventario con la demanda de bienes.jpeg"
+                  cover></v-img>
 
-              <v-img height="250"
-                src="public/el concepto del sistema de control de inventario, el gerente profesional y el trabajador están verificando el suministro de bienes y existencias, la gestión de inventario con la demanda de bienes.jpeg"
-                cover></v-img>
+                <v-card-item>
+                  <v-card-title>Plan Avanzado</v-card-title>
 
-              <v-card-item>
-                <v-card-title>Plan Avanzado</v-card-title>
+                  <v-card-subtitle>
+                    <span class="me-1">Popular</span>
 
-                <v-card-subtitle>
-                  <span class="me-1">Popular</span>
+                    <v-icon color="error" icon="mdi-fire-circle" size="small"></v-icon>
+                  </v-card-subtitle>
+                </v-card-item>
 
-                  <v-icon color="error" icon="mdi-fire-circle" size="small"></v-icon>
-                </v-card-subtitle>
-              </v-card-item>
+                <v-card-text>
+                  <v-row align="center" class="mx-0">
+                    <v-rating :model-value="5" color="amber" density="compact" size="small" half-increments
+                      readonly></v-rating>
 
-              <v-card-text>
-                <v-row align="center" class="mx-0">
-                  <v-rating :model-value="5" color="amber" density="compact" size="small" half-increments
-                    readonly></v-rating>
+                    <div class="text-grey ms-4">
+                      5 (678)
+                    </div>
+                  </v-row>
 
-                  <div class="text-grey ms-4">
-                    5 (678)
+                  <div class="my-4 text-subtitle-1">
+                    10$ • La paz, Bolivia
                   </div>
-                </v-row>
 
-                <div class="my-4 text-subtitle-1">
-                  10$ • La paz, Bolivia
-                </div>
+                  <div>Sistema avanzado para rastrear, gestionar y optimizar inventario empresarial.</div>
+                </v-card-text>
 
-                <div>Sistema avanzado para rastrear, gestionar y optimizar inventario empresarial.</div>
-              </v-card-text>
+                <v-divider class="mx-4 mb-1"></v-divider>
 
-              <v-divider class="mx-4 mb-1"></v-divider>
+                <v-card-actions>
+                  <v-btn color="deep-purple-lighten-2" text="Comprar" block border @click="reserve"></v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
 
-              <v-card-actions>
-                <v-btn color="deep-purple-lighten-2" text="Comprar" block border @click="reserve"></v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-col>
+            <v-col cols="auto" class="card-col">
+              <v-card :disabled="loading" :loading="loading" max-width="374">
+                <template v-slot:loader="{ isActive }">
+                  <v-progress-linear :active="isActive" color="deep-purple" height="4" indeterminate></v-progress-linear>
+                </template>
 
-          <v-col cols="auto" class="card-col">
-            <v-card :disabled="loading" :loading="loading" max-width="374">
-              <template v-slot:loader="{ isActive }">
-                <v-progress-linear :active="isActive" color="deep-purple" height="4" indeterminate></v-progress-linear>
-              </template>
+                <v-img height="250"
+                  src="public/el concepto del sistema de control de inventario, el gerente profesional y el trabajador están verificando el suministro de bienes y existencias, la gestión de inventario con la demanda de bienes.jpeg"
+                  cover></v-img>
 
-              <v-img height="250"
-                src="public/el concepto del sistema de control de inventario, el gerente profesional y el trabajador están verificando el suministro de bienes y existencias, la gestión de inventario con la demanda de bienes.jpeg"
-                cover></v-img>
+                <v-card-item>
+                  <v-card-title>Plan Premium</v-card-title>
 
-              <v-card-item>
-                <v-card-title>Plan Premium</v-card-title>
+                  <v-card-subtitle>
+                    <span class="me-1">Nuevo</span>
 
-                <v-card-subtitle>
-                  <span class="me-1">Nuevo</span>
+                    <v-icon color="error" icon="mdi-fire-circle" size="small"></v-icon>
+                  </v-card-subtitle>
+                </v-card-item>
 
-                  <v-icon color="error" icon="mdi-fire-circle" size="small"></v-icon>
-                </v-card-subtitle>
-              </v-card-item>
+                <v-card-text>
+                  <v-row align="center" class="mx-0">
+                    <v-rating :model-value="4.8" color="amber" density="compact" size="small" half-increments
+                      readonly></v-rating>
 
-              <v-card-text>
-                <v-row align="center" class="mx-0">
-                  <v-rating :model-value="4.8" color="amber" density="compact" size="small" half-increments
-                    readonly></v-rating>
+                    <div class="text-grey ms-4">
+                      4.8 (530)
+                    </div>
+                  </v-row>
 
-                  <div class="text-grey ms-4">
-                    4.8 (530)
+                  <div class="my-4 text-subtitle-1">
+                    15$ • La paz, Bolivia
                   </div>
-                </v-row>
 
-                <div class="my-4 text-subtitle-1">
-                  15$ • La paz, Bolivia
-                </div>
+                  <div>Sistema premium para rastrear, gestionar y optimizar inventario empresarial con características adicionales.</div>
+                </v-card-text>
 
-                <div>Sistema premium para rastrear, gestionar y optimizar inventario empresarial con características adicionales.</div>
-              </v-card-text>
+                <v-divider class="mx-4 mb-1"></v-divider>
 
-              <v-divider class="mx-4 mb-1"></v-divider>
+                <v-card-actions>
+                  <v-btn color="deep-purple-lighten-2" text="Comprar" block border @click="reserve"></v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </div>
+    </v-app>
+  </template>
 
-              <v-card-actions>
-                <v-btn color="deep-purple-lighten-2" text="Comprar" block border @click="reserve"></v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-    </div>
-  </v-app>
-</template>
+  <script>
+  import { useRouter } from 'vue-router'
 
-<script>
-import { useRouter } from 'vue-router'
+  export default {
+    data: () => ({
+      loading: false,
+      selection: 1,
+    }),
 
-export default {
-  data: () => ({
-    loading: false,
-    selection: 1,
-  }),
+    setup() {
+      const router = useRouter()
 
-  setup() {
-    const router = useRouter()
+      const redirectTo = (path) => {
+        router.push(path)
+      }
 
-    const redirectTo = (path) => {
-      router.push(path)
-    }
+      return {
+        redirectTo,
+        reserve() {
+          this.loading = true
+          setTimeout(() => (this.loading = false), 2000)
+        },
+      }
+    },
+  }
+  </script>
 
-    return {
-      redirectTo,
-      reserve() {
-        this.loading = true
-        setTimeout(() => (this.loading = false), 2000)
-      },
-    }
-  },
-}
-</script>
-
-<style scoped>
-.fill-height {
-  height: 100vh;
-}
-.andale-mono {
-  font-family: 'Andale Mono', monospace;
-}
-.card-col {
-  margin: 0 0.7rem;
-}
-</style>
+  <style scoped>
+  .fill-height {
+    height: 100vh;
+  }
+  .andale-mono {
+    font-family: 'Andale Mono', monospace;
+  }
+  .card-col {
+    margin: 0 0.7rem;
+  }
+  </style>
